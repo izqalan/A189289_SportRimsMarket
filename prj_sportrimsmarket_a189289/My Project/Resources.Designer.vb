32a5b36dfb4b90478a15aba50b417aad01d76046
izqalan <izqalan@gmail.com>
2022-01-27 22:30:19 +0800

    bug fix

--- prj_sportrimsmarket_a189289/My Project/Resources.Designer.vb
+++ prj_sportrimsmarket_a189289/My Project/Resources.Designer.vb
@@ -71,6 +71,16 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        '''</summary>
+        Friend ReadOnly Property f1_yellow_wheel() As System.Drawing.Icon
+            Get
+                Dim obj As Object = ResourceManager.GetObject("f1_yellow_wheel", resourceCulture)
+                Return CType(obj,System.Drawing.Icon)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
         Friend ReadOnly Property porsche() As System.Drawing.Bitmap
